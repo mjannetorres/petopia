1957396c06ebcdf3b31f0f29351ccd167cc6f93f
torreschin <maryjanne@sellerinvestigators.com>
2025-02-12 23:32:54 +0800

    Sign up error validation

--- app/(auth)/sign-up/sign-up-form.tsx
+++ app/(auth)/sign-up/sign-up-form.tsx
@@ -40,7 +40,6 @@ const SignUpForm = () => {
             id='name'
             name='name'
             type='text'
-            required
             autoComplete='name'
             defaultValue={signUpDefaultValues.name}
           />
@@ -50,8 +49,7 @@ const SignUpForm = () => {
           <Input
             id='email'
             name='email'
-            type='email'
-            required
+            type='text'
             autoComplete='email'
             defaultValue={signUpDefaultValues.email}
           />
