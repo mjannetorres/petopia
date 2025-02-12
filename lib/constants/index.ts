5a1ab76e407530271e16d7a49336b35a7de5b85f
torreschin <maryjanne@sellerinvestigators.com>
2025-02-12 21:45:55 +0800

    sign in form with auth

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -4,3 +4,8 @@ export const APP_DESCRIPTION =
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT = process.env.LATEST_PRODUCTS_LIMIT || 4;
+
+export const signInDefaultValues = {
+  email: "",
+  password: "",
+};
