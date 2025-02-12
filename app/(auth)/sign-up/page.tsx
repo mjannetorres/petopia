b49131aa1e6aaef78262bcb694356b4eb0811ddc
torreschin <maryjanne@sellerinvestigators.com>
2025-02-12 23:14:58 +0800

    Sign up form

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,58 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import { APP_NAME } from "@/lib/constants";
+import { redirect } from "next/navigation";
+import { auth } from "@/auth";
+import SignUpForm from "./sign-up-form";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+const SinUpPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+
+  return (
+    <div className='w-full max-w-md mx-auto'>
+      <Card>
+        <CardHeader className='space-y-4'>
+          <Link href='/' className='flex-center'>
+            <Image
+              src='/images/logo.svg'
+              width={100}
+              height={100}
+              alt={`${APP_NAME}`}
+              priority={true}
+            />
+          </Link>
+          <CardTitle className='text-center'>Create Account</CardTitle>
+          <CardDescription className='text-center'>
+            Enter your information below to sign up
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='space-y-4'>
+          <SignUpForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default SinUpPage;
